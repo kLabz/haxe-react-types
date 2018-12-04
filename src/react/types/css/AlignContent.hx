fda6ecb1407e9761eaa67ad0fa3d529880ff02b9
k <k@klabz.org>
2018-12-04 08:35:15 +0100

    Update existing enums

--- src/react/types/css/AlignContent.hx
+++ src/react/types/css/AlignContent.hx
@@ -1,11 +1,19 @@
 package react.types.css;
 
-// TODO: complete values from https://developer.mozilla.org/en-US/docs/Web/CSS/align-content
-@:enum abstract AlignContent(String) to String {
-	var Stretch = "stretch";
+@:enum abstract AlignContent(String) from GlobalValue to String {
 	var Center = "center";
+	var Start = "start";
+	var End = "end";
 	var FlexStart = "flex-start";
 	var FlexEnd = "flex-end";
+	var Normal = "normal";
+	var Baseline = "baseline";
+	var FirstBaseline = "first baseline";
+	var LastBaseline = "last baseline";
 	var SpaceBetween = "space-between";
 	var SpaceAround = "space-around";
+	var SpaceEvenly = "space-evenly";
+	var Stretch = "stretch";
+	var SafeCenter = "safe center";
+	var UnsafeCenter = "unsafe center";
 }
--- src/react/types/css/JustifyContent.hx
+++ src/react/types/css/JustifyContent.hx
@@ -1,11 +1,18 @@
 package react.types.css;
 
-// TODO: complete values from https://developer.mozilla.org/en-US/docs/Web/CSS/justify-content
-@:enum abstract JustifyContent(String) to String {
-	var FlexStart = "flex-start";
+@:enum abstract JustifyContent(String) from GlobalValue to String {
 	var Center = "center";
+	var Start = "start";
+	var End = "end";
+	var FlexStart = "flex-start";
 	var FlexEnd = "flex-end";
+	var Left = "left";
+	var Right = "right";
+	var Normal = "normal";
 	var SpaceBetween = "space-between";
 	var SpaceAround = "space-around";
 	var SpaceEvenly = "space-evenly";
+	var Stretch = "stretch";
+	var SafeCenter = "safe center";
+	var UnsafeCenter = "unsafe center";
 }
