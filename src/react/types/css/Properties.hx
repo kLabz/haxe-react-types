673a6d4a89761843e7ec1e376596b221677ed1c5
k <k@klabz.org>
2018-12-04 16:04:35 +0100

    Added VerticalAlign

--- src/react/types/css/Properties.hx
+++ src/react/types/css/Properties.hx
@@ -312,7 +312,7 @@ typedef Properties = {
   @:optional var transitionProperty(default, never):String;
   @:optional var transitionTimingFunction(default, never):String;
   @:optional var unicodeBidi(default, never):String;
-  @:optional var verticalAlign(default, never):String;
+  @:optional var verticalAlign(default, never):VerticalAlign;
   @:optional var visibility(default, never):String;
   @:optional var voiceBalance(default, never):String;
   @:optional var voiceDuration(default, never):String;
