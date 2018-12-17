22df3e26e1316b8a14f845162c25292ef546296f
k <k@klabz.org>
2018-12-17 21:44:02 +0100

    RecordMacro: try caching Record<T>

--- src/react/types/macro/RecordMacro.hx
+++ src/react/types/macro/RecordMacro.hx
@@ -5,6 +5,9 @@ import haxe.macro.Context;
 import haxe.macro.Type;
 
 class RecordMacro {
+	// Note: this will only be persistent in haxe 4 preview 5 and newer
+	@:persistent static var recordCache:Map<String, Array<Field>> = new Map();
+
 	public static function build():ComplexType {
 		return buildRecord(
 			Context.getLocalType(),
@@ -19,34 +22,41 @@ class RecordMacro {
 		valueType:ComplexType,
 		?asPartial:Bool = false
 	):ComplexType {
-		var classes = switch (abstractEnumType) {
+		switch (abstractEnumType) {
 			case TInst(_, [TAbstract(_.get() => t, params)]):
-				t.impl.get().statics.get();
+				var cacheKey = '${t.module}.${t.name}';
+				if (recordCache.exists(cacheKey))
+					return TAnonymous(recordCache.get(cacheKey));
+
+				var classes = t.impl.get().statics.get();
+
+				var meta = asPartial
+					? [{name: ":optional", params: null, pos: Context.currentPos()}]
+					: null;
+
+				var fields:Array<Field> = classes.map(function(c):Field {
+					if (c.expr() == null) return null;
+
+					// TODO: check if it matches with valueType
+					return switch (c.expr().expr) {
+						case TCast({expr: TConst(TString(name))}, _):
+							{
+								kind: FVar(valueType, null),
+								name: name,
+								meta: meta == null ? null : meta.copy(),
+								pos: Context.currentPos()
+							};
+
+						// TODO: support other types
+						default: null;
+					}
+				}).filter(function(f) return f != null);
+
+				recordCache.set(cacheKey, fields);
+				return TAnonymous(fields);
 
 			default:
 				throw 'buildRecord() should be called with an abstract enum';
 		};
-
-		var meta = asPartial
-			? [{name: ":optional", params: null, pos: Context.currentPos()}]
-			: null;
-
-		var fields:Array<Field> = classes.map(function(c):Field {
-			// TODO: check if it matches with valueType
-			return switch (c.expr().expr) {
-				case TCast({expr: TConst(TString(name))}, _):
-					{
-						kind: FVar(valueType, null),
-						name: name,
-						meta: meta == null ? null : meta.copy(),
-						pos: Context.currentPos()
-					};
-
-				// TODO: support other types
-				default: null;
-			}
-		});
-
-		return TAnonymous(fields);
 	}
 }
