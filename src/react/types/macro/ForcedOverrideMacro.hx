712cf63bd22ea3dceb2ed992848f2e52d75ecffb
Rudy G <k@klabz.org>
2020-01-16 11:17:17 +0100

    ForcedOverride: add dev tools

--- src/react/types/macro/ForcedOverrideMacro.hx
+++ src/react/types/macro/ForcedOverrideMacro.hx
@@ -1,5 +1,6 @@
 package react.types.macro;
 
+import haxe.macro.ComplexTypeTools;
 import haxe.macro.Context;
 import haxe.macro.Expr;
 import haxe.macro.Type;
@@ -11,8 +12,40 @@ class ForcedOverrideMacro {
 			case TInst(_, [t1, t2]):
 				var fields = extractFields(t1);
 				var newFields = extractFields(t2);
+				var overrides = 0;
 
-				for (f in newFields.keys()) fields.set(f, newFields.get(f));
+				// TODO: document these somewhere:
+				var reportUnneeded = Context.defined('react_types_report_unneeded_field_override');
+				var reportUnneededFields = Context.defined('react_types_report_unneeded_field_override');
+				var reportFieldOverride = Context.defined('react_types_report_field_override');
+				var checkOverrides = reportUnneeded || reportUnneededFields || reportFieldOverride;
+
+				for (f in newFields.keys()) {
+					var field = newFields.get(f);
+
+					if (fields.exists(f)) {
+						var baseField = fields.get(f);
+
+						if (checkOverrides) {
+							if (!compareFieldsTypes(field, baseField)) {
+								overrides++;
+
+								if (reportFieldOverride) {
+									Context.warning('Overriding this field', field.pos);
+									Context.warning('Base field is defined here', baseField.pos);
+								}
+							} else if (reportUnneededFields) {
+								Context.warning('Overriding field `${field.name}` with same type as base field', field.pos);
+							}
+						}
+					}
+
+					fields.set(f, field);
+				}
+
+				if (reportUnneeded && overrides == 0) {
+					Context.warning('ForcedOverride is not needed here', Context.currentPos());
+				}
 
 				return TAnonymous(Lambda.array(fields));
 
@@ -34,6 +67,20 @@ class ForcedOverrideMacro {
 					'ForcedOverride: structure expected',
 					Context.currentPos()
 				);
+		};
+	}
+
+	static function compareFieldsTypes(f1:Field, f2:Field):Bool {
+		var t1 = ComplexTypeTools.toString(extractFieldType(f1));
+		var t2 = ComplexTypeTools.toString(extractFieldType(f2));
+		return t1 == t2;
+	}
+
+	static function extractFieldType(f:Field):ComplexType {
+		return switch (f.kind) {
+			case FVar(ct, _): ct;
+			case FFun(_): throw 'Not supported for functions atm';
+			case FProp(_, _, ct, _): ct;
 		};
 	}
 }
